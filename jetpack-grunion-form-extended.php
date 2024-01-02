9257b536d063cd80fbc2b29a26862ef55d58c63d
Nerds Farm <info@nerds.farm>
2024-01-02 11:42:02 +0100

    start

---
+++ jetpack-grunion-form-extended.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+Plugin Name: JetPack Grunion Contact Form Extended
+Description: Add a contact form to any post, page or text widget.  Emails will be sent to the post's author by default, or any email address you choose.  As seen on WordPress.com.
+Plugin URI: http://automattic.com/#
+AUthor: Automattic, Inc.
+Author URI: http://automattic.com/
+Version: 1.0.1
+License: GPLv3 or later
+*/
+
+add_action( 'plugins_loaded', function() {
+    $jetpack_form_dir = 'JETPACK__PLUGIN_DIR'; // GRUNION_PLUGIN_DIR
+    if ( defined( $jetpack_form_dir ) ) {
+        // TODO
+        //https://developer.jetpack.com/module/contact-form/
+        add_filter('render_block',  function($block_content, $parsed_block, $block) {
+            //var_dump($block->name);
+            if ($block->name == 'jetpack/contact-form') {
+                $block_content = str_replace('wp-block-jetpack-contact-form', 'wp-block-jetpack-contact-form wp-block-jetpack-contact-form-ext', $block_content);
+            }
+            //jetpack/field-
+            return $block_content;
+        }, 10, 3 );
+    } else {
+        add_action( 'admin_notices', 'grunion_form_plugin_fail_load' );
+    }
+});
+
+/**
+ * Show in WP Dashboard notice about the plugin is not activated.
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+function grunion_form_plugin_fail_load() {
+	$screen = get_current_screen();
+	if ( isset( $screen->parent_file ) && 'plugins.php' === $screen->parent_file && 'update' === $screen->id ) {
+		return;
+	}
+
+	$plugin = 'jetpack/jetpack.php';
+        $installed_plugins = get_plugins();
+	if ( isset( $installed_plugins[ $plugin ] ) ) {
+		if ( ! current_user_can( 'activate_plugins' ) ) {
+			return;
+		}
+
+		$activation_url = wp_nonce_url( 'plugins.php?action=activate&amp;plugin=' . $plugin . '&amp;plugin_status=all&amp;paged=1&amp;s', 'activate-plugin_' . $plugin );
+
+		$message = '<h3>' . esc_html__( 'You\'re not using Grunion Form Extended yet!', 'elementor-pro' ) . '</h3>';
+		$message .= '<p>' . esc_html__( 'Activate the JetPack plugin to start using all of Grunion Form Extended plugin’s features.', 'elementor-pro' ) . '</p>';
+		$message .= '<p>' . sprintf( '<a href="%s" class="button-primary">%s</a>', $activation_url, esc_html__( 'Activate Now', 'elementor-pro' ) ) . '</p>';
+	} else {
+		if ( ! current_user_can( 'install_plugins' ) ) {
+			return;
+		}
+
+		$install_url = wp_nonce_url( self_admin_url( 'update.php?action=install-plugin&plugin=jetpack' ), 'install-plugin_jetpack' );
+
+		$message = '<h3>' . esc_html__( 'Grunion Form Extended plugin requires installing the JetPack plugin', 'elementor-pro' ) . '</h3>';
+		$message .= '<p>' . esc_html__( 'Install and activate the JetPack plugin to access all the features.', 'elementor-pro' ) . '</p>';
+		$message .= '<p>' . sprintf( '<a href="%s" class="button-primary">%s</a>', $install_url, esc_html__( 'Install Now', 'elementor-pro' ) ) . '</p>';
+	}
+
+	echo '<div class="error">' . $message . '</div>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+}
---
+++ modules/grunion-contact-form.php
@@ -0,0 +1,20 @@
+<?php
+
+use Automattic\Jetpack\Assets;
+use Automattic\Jetpack\Blocks;
+use Automattic\Jetpack\Sync\Settings;
+
+/**
+ * Description of grunion-contact-form
+ *
+ * @author Fra
+ */
+class Grunion_Contact_Form_Extended {
+    
+    
+    public function __construct( $attributes, $content = null, $form = null ) {
+        $plugin = Grunion_Contact_Form_Plugin::init();
+    }
+    
+//put your code here
+}
